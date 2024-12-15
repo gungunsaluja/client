000f534a668ae69f17c5d8df75085bfd6bda1f31
gungunsaluja <gungunsaluja16@gmail.com>
2024-12-16 02:16:24 +0530

    pages

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,6 +4,15 @@ const RegisterPage = () =>{
         <div className = 'register'>
             <div className = 'register_content'>
                 <form>
+                    <input placeholder = "First Name" name = "firstname" required
+                    ></input>
+                    <input placeholder = "Last Name" name = "lastname" required
+                    ></input>
+                    <input placeholder = "Email" type = "email" name = "email" required 
+                    ></input><input placeholder = "Password" name = "password" type = "password" required
+                    ></input><input placeholder = "Confirm Password" name = "confirmPassword" type = "password" required 
+                    ></input>
+                    <input type = "file" name = "profileImage" accept = "image/" required style = {{display:'none'}} ></input>
 
                 </form>
                 <a href = '/login'>Already have an account ? Log In Here
