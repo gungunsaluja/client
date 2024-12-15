f45a751a1ae80d9edde99836baad532e420b65f9
gungunsaluja <gungunsaluja16@gmail.com>
2024-12-16 01:22:22 +0530

    dream nest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<HomePage></HomePage>}/>
+      <Route path = "/register" element = {<RegisterPage></RegisterPage>}/>
+      <Route path = "/login" element = {<LoginPage></LoginPage>}/>
+      
+      
+        </Routes>
+
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const RegisterPage = () =>{
+    return (
+        <div className = 'register'>
+            <div className = 'register_content'>
+                <form>
+
+                </form>
+                <a href = '/login'>Already have an account ? Log In Here
+
+                </a>
+            </div>
+            RegisterPage
+
+        </div>
+    )
+}
+export default RegisterPage;
