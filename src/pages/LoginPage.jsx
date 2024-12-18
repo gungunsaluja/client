63670aabc70a12380f79f898937dd8599ad9835a
gungunsaluja <gungunsaluja16@gmail.com>
2024-12-18 22:44:49 +0530

    login page styling

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "../styles/Login.scss"
 const LoginPage = () =>{
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+
     return (
         <div className = 'login'>
             <div className = 'login_content'>
                 <form action = "login_content_form">
+
+                    <input type = "email" placeholder = 'Email'  value = {email} onChange={(e)=>e.target.value}
+                    required ></input>
+                    <input type = "password" placeholder = "password" required ></input>
+                    <button type = "submit">Log In</button>
                 </form>
-                <a href = ""></a>
+                <a href = "/register">Don't have an account ? Sign In Here</a>
             </div>
 
 
