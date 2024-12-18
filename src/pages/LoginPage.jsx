1408e2457faf673bd07406686cf3ac6dc0c6b302
gungunsaluja <gungunsaluja16@gmail.com>
2024-12-18 22:34:35 +0530

    commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 const LoginPage = () =>{
     return (
-        <div></div>
+        <div className = 'login'>
+            <div className = 'login_content'>
+                <form action = "login_content_form">
+                </form>
+                <a href = ""></a>
+            </div>
+
+
+
+        </div>
     )
 }
 export default LoginPage;
